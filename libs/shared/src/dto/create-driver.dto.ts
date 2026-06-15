829bc5cc17d33d44112ccf0af0da319eed438e5e
tiennnk <kieutiennguyenngoc2002.tn@gmail.com>
2026-06-15 16:55:46 +0700

    [tiennnk][20260615][feat] add ApiProperty decorators to shared DTOs

--- libs/shared/src/dto/create-driver.dto.ts
+++ libs/shared/src/dto/create-driver.dto.ts
@@ -1,17 +1,22 @@
 import { IsString, IsEmail, IsEnum, IsOptional } from 'class-validator';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { DriverType } from '../types/driver.types';
 
 export class CreateDriverDto {
+  @ApiProperty({ example: 'Tien Driver' })
   @IsString()
   name!: string;
 
+  @ApiProperty({ example: '0323456789' })
   @IsString()
   phone!: string;
 
+  @ApiPropertyOptional({ example: 'driver@example.com' })
   @IsEmail()
   @IsOptional()
   email?: string;
 
+  @ApiProperty({ enum: DriverType })
   @IsEnum(DriverType)
   type?: DriverType;
 }
--- libs/shared/src/dto/update-driver-status.dto.ts
+++ libs/shared/src/dto/update-driver-status.dto.ts
@@ -1,7 +1,9 @@
 import { IsEnum } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 import { DriverStatus } from '../types/driver.types';
 
 export class UpdateDriverStatusDto {
+  @ApiProperty({ enum: DriverStatus })
   @IsEnum(DriverStatus)
   status?: DriverStatus;
 }
